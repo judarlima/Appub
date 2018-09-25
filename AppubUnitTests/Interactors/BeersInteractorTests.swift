9b9773e57adef537149555b92216a6cd9ed34171
Judar Lima <judar.rslima@gmail.com>
2018-09-25 02:14:27 -0300

    Create mock files.

--- AppubUnitTests/Interactors/BeersInteractorTests.swift
+++ AppubUnitTests/Interactors/BeersInteractorTests.swift
@@ -1,75 +1,18 @@
-//
-//  BeersInteractorTests.swift
-//  AppubUnitTests
-//
-//  Created by Judar Lima on 9/24/18.
-//  Copyright © 2018 Raduj. All rights reserved.
-//
-
 import XCTest
 @testable import Appub
 
-class MockGateway: BeersGatewayProtocol {
-  var isFailure = false
-  var expectedIndex = 0
-  
-  func getAllBeers(completion: @escaping (Result<[Beer]>) -> Void) {
-    if isFailure {
-      completion(Result.fail(.couldNotFoundURL))
-    } else {
-      completion(Result.success([Beer]()))
-    }
-  }
-  func getBeer(with id: Int, completion: @escaping (Result<Beer>) -> Void) {
-    expectedIndex = id
-    if isFailure {
-      completion(Result.fail(.couldNotFoundURL))
-    } else {
-      completion(Result.success(Beer(id: 5,
-                                     name: "",
-                                     tagline: "",
-                                     description: "",
-                                     imageURL: "",
-                                     abv: 0,
-                                     ibu: 0)))
-    }
-  }
-}
-
-class MockPresenter: BeersListPresenter {
-  var errorWasPresented: Bool = false
-  var beerListWasPresented: Bool = false
-  
-  func showError(error: Error?) {
-    errorWasPresented = true
-  }
-  
-  func showBeerList(beers: [BeerCollectionViewModel]) {
-    beerListWasPresented = true
-  }
-  
-}
-
-class MockRouter: BeerListRouterProtocol {
-  var routeToDetailWasCalled = false
-  
-  func routeToBeerDetails(with viewModel: BeerDetailViewModel) {
-    routeToDetailWasCalled = true
-  }
-}
-
 class BeersInteractorTests: XCTestCase {
   var sut: BeersInteractor!
-  var gateway: MockGateway!
-  var presenter: MockPresenter!
-  var router: MockRouter!
+  var gateway: BeerGatewayMock!
+  var presenter: BeerPresenterMock!
+  var router: BeerRouterMock!
   
   override func setUp() {
     
     super.setUp()
-    gateway = MockGateway()
-    presenter = MockPresenter()
-    router = MockRouter()
+    gateway = BeerGatewayMock()
+    presenter = BeerPresenterMock()
+    router = BeerRouterMock()
     sut = BeersInteractor(gateway: gateway, presenter: presenter, router: router)
   }
   
