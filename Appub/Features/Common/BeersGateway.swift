7c3f7f9fa49ed833c71cba1d63324ad3cb55255e
Judar Lima <judar.rslima@gmail.com>
2018-09-24 22:06:16 -0300

    Refactor generateResult.

--- Appub/Features/Common/BeersGateway.swift
+++ Appub/Features/Common/BeersGateway.swift
@@ -15,44 +15,40 @@ struct BeersGateway: BeersGatewayProtocol {
   
   func getAllBeers(completion: @escaping (Result<[Beer]>) -> Void) {
     service.requestData(with: BeersGatewaySetup.allBeers) { (responseData, responseError) in
-      if let error = responseError {
-        completion(Result.fail(ServiceError.failure(error.localizedDescription)))
-      }
-      else if let data = responseData {
-        do {
-          let jsonDecoder = JSONDecoder()
-          let response = try jsonDecoder.decode([Beer].self, from: data)
-          completion(Result.success(response))
-        } catch let error {
-          completion(Result.fail(ServiceError.unknown(error.localizedDescription)))
-        }
-      } else {
-        completion(Result.fail(ServiceError.couldNotParseResponse))
-      }
+      completion(self.generateResult(responseData: responseData, responseError: responseError))
     }
   }
   
   func getBeer(with id: Int, completion: @escaping (Result<Beer>) -> Void) {
     service.requestData(with: BeersGatewaySetup.singleBeer(id: id)) { (responseData, responseError) in
-      if let error = responseError {
-        completion(Result.fail(ServiceError.failure(error.localizedDescription)))
-      }
-      else if let data = responseData {
-        do {
-          let jsonDecoder = JSONDecoder()
-          let response = try jsonDecoder.decode([Beer].self, from: data)
-          guard let beer = response.first
-            else {
-              completion(Result.fail(ServiceError.couldNotParseResponse))
-              return
-          }
-          completion(Result.success(beer))
-        } catch let error {
-          completion(Result.fail(ServiceError.unknown(error.localizedDescription)))
+      let result = self.generateResult(responseData: responseData, responseError: responseError)
+      switch result {
+      case let .success(beerArray):
+        guard let beer = beerArray.first
+          else {
+            completion(Result.fail(ServiceError.couldNotParseResponse))
+            return
         }
-      } else {
-        completion(Result.fail(ServiceError.couldNotParseResponse))
+        completion(Result.success(beer))
+      case let .fail(error): completion(Result.fail(error))
       }
+    }
+  }
+  
+  private func generateResult(responseData: Data?, responseError: ServiceError?) -> Result<[Beer]> {
+    if let error = responseError {
+      return Result.fail(ServiceError.failure(error.localizedDescription))
+    }
+    else if let data = responseData {
+      do {
+        let jsonDecoder = JSONDecoder()
+        let response = try jsonDecoder.decode([Beer].self, from: data)
+        return Result.success(response)
+      } catch let error {
+        return Result.fail(ServiceError.unknown(error.localizedDescription))
+      }
+    } else {
+      return Result.fail(ServiceError.couldNotParseResponse)
     }
   }
 }
