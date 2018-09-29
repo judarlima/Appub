b3f55fecf40d25145b16d47b34a0b0e03284fe04
Judar Lima <judar.rslima@gmail.com>
2018-09-29 17:35:11 -0300

    Fix files identation.

--- AppubUnitTests/Interactor/BeersInteractorTests.swift
+++ AppubUnitTests/Interactor/BeersInteractorTests.swift
@@ -2,52 +2,52 @@ import XCTest
 @testable import Appub
 
 class BeersInteractorTests: XCTestCase {
-  var sut: BeersInteractor!
-  var gateway: BeerGatewayMock!
-  var presenter: BeerPresenterMock!
-  var router: BeerRouterMock!
-  
-  override func setUp() {
+    var sut: BeersInteractor!
+    var gateway: BeerGatewayMock!
+    var presenter: BeerPresenterMock!
+    var router: BeerRouterMock!
     
-    super.setUp()
-    gateway = BeerGatewayMock()
-    presenter = BeerPresenterMock()
-    router = BeerRouterMock()
-    sut = BeersInteractor(gateway: gateway, presenter: presenter, router: router)
-  }
-  
-  func testGetBeerListWhenGatewayReturnsSucessResultThenPresentBeerList() {
-    sut.beerList()
+    override func setUp() {
+        
+        super.setUp()
+        gateway = BeerGatewayMock()
+        presenter = BeerPresenterMock()
+        router = BeerRouterMock()
+        sut = BeersInteractor(gateway: gateway, presenter: presenter, router: router)
+    }
     
-    XCTAssertTrue(presenter.beerListWasPresented)
-    XCTAssertFalse(presenter.errorWasPresented)
-  }
-  
-  func testGetBeerListWhenGatewayReturnsFailResultThenPresentError() {
-    gateway.isFailure = true
+    func testGetBeerListWhenGatewayReturnsSucessResultThenPresentBeerList() {
+        sut.beerList()
+        
+        XCTAssertTrue(presenter.beerListWasPresented)
+        XCTAssertFalse(presenter.errorWasPresented)
+    }
     
-    sut.beerList()
+    func testGetBeerListWhenGatewayReturnsFailResultThenPresentError() {
+        gateway.isFailure = true
+        
+        sut.beerList()
+        
+        XCTAssertTrue(presenter.errorWasPresented)
+        XCTAssertFalse(presenter.beerListWasPresented)
+    }
     
-    XCTAssertTrue(presenter.errorWasPresented)
-    XCTAssertFalse(presenter.beerListWasPresented)
-  }
-  
-  func testGetBeerWhenGatewayReturnsSuccessResultThenRouteToDetail() {
-    let beerId = "10"
+    func testGetBeerWhenGatewayReturnsSuccessResultThenRouteToDetail() {
+        let beerId = "10"
+        
+        sut.beer(with: beerId)
+        
+        XCTAssertTrue(router.routeToDetailWasCalled)
+        XCTAssertEqual(beerId, gateway.expectedId)
+    }
     
-    sut.beer(with: beerId)
+    func testGetBeerWhenGatewayReturnsFailResultThenPresentError() {
+        gateway.isFailure = true
+        let beerId = "22"
+        sut.beer(with: beerId)
+        
+        XCTAssertTrue(presenter.errorWasPresented)
+        XCTAssertFalse(router.routeToDetailWasCalled)
+    }
     
-    XCTAssertTrue(router.routeToDetailWasCalled)
-    XCTAssertEqual(beerId, gateway.expectedId)
-  }
-  
-  func testGetBeerWhenGatewayReturnsFailResultThenPresentError() {
-    gateway.isFailure = true
-    let beerId = "22"
-    sut.beer(with: beerId)
-    
-    XCTAssertTrue(presenter.errorWasPresented)
-    XCTAssertFalse(router.routeToDetailWasCalled)
-  }
-  
 }
